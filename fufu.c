6b64a9d4e1c15cac739137c791be9e5f80f8f244
gkosnia163 <niafaskonstantinos@gmail.com>
2025-05-27 23:35:36 +0300

    insert - stdin //douleyei kanonika

--- fufu.c
+++ fufu.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 typedef struct wList 
 { 
     char *word;      // the stored word 
-    //int wordLength;  // length of the word in letters 
-    //int uniqLetters; // number of unique word letters 
-    //int numOfTimes;  // no of times a word appears in the text 
+    int wordLength;  // length of the word in letters 
+    int uniqLetters; // number of unique word letters 
+    int numOfTimes;  // no of times a word appears in the text 
     int insertionMode; //0 from file, 1 from keyboard 
     //bale time kai date p paizeis
     struct wList *next; 
@@ -21,6 +22,8 @@ typedef wordList *wlPtr;
 void insert(wordList **wlPtr, char* input, int insertionMode);
 int elenxos_input(char* input);
 void printList(wl *head);
+int wlenght(char* word);
+int uniql(char* word);
 
 int main()
 {
@@ -30,10 +33,15 @@ int main()
     while(1){
     printf("\ncmds: insert / delete / exit\n$>");  
     scanf("%s", input);
+
     switch(elenxos_input(input)){
         case 1:
-            scanf("%s", input);
-            insert(&head, input, 1);
+            printf("$> insert ");
+            while(getchar() != '\n');
+                if (fgets(input, 100, stdin) != NULL){
+                    input[strcspn(input, "\n")] = '\0';
+                    insert(&head, input, 1);
+                } else printf("error read\n");
             printList(head);
             break;
         case 2:
@@ -60,41 +68,106 @@ int elenxos_input(char* input){
     else return 0;
 }
 
-void insert(wl **wlPtr, char* input, int insertionMode){
-    
-    wl *newnode = (wl*)malloc(sizeof(wl));
-
-    if(newnode != NULL) {
-        newnode->word = strdup(input); //duplicate
-        newnode->next = NULL;
-        newnode->prev = NULL;
-        newnode->insertionMode = 1;
-
-        if(wlPtr == NULL){
-            *wlPtr = newnode;
-            return;
-        }
-
-        wl *current = *wlPtr;
-        wl *prev = NULL;
-
-        while(current != NULL && strcmp(input, current->word) > 0){
-            prev = current;
-            current = current->next;
-        }
-
-        newnode->next = current;
-        newnode->prev = prev;
-
-        if(prev == NULL) *wlPtr = newnode;
-        else prev->next = newnode;
-        if(current != NULL)
-            current->prev = newnode;
-    }else{
-        printf( "%c not inserted. No memory available.\n", input );
-        return;
-    }
+int notletter(char c) {
+    return  c == ' ' || c == '\r' || c == '\0' ||
+            c == ',' || c == '.' || c == '!' || c == '?'||
+            c == ';' || c == ':';
 }
+
+void insert(wl **wlPtr, char* input, int insertionMode){
+    if(input == NULL || strlen(input) == 0) return;//ftiakse elenxo swsta ama input einai enter sketo
+
+    int i = 0, start = 0, end = 0;
+    int len = strlen(input);
+
+    while(end <= len){
+        if(notletter(input[end])){ 
+            if(start < end){
+                
+                char cleaned[172] = {0}; //megalyterh leksh ths ellhnikhs pali
+                int j = 0;
+                
+                for(i = start; i < end; i++){ //afhnei mono grammata
+                    if(isalpha((unsigned char)input[i])){
+                        cleaned[j++] = toupper(input[i]);
+                    }
+                }
+                cleaned[j] = '\0'; ////////
+            
+                if(j>0){
+                    wl *current = *wlPtr;
+                    while(current != NULL){
+                        if(strcmp(cleaned, current -> word) == 0){
+                            current->numOfTimes++;   
+                            break;
+                        }
+                        current = current->next;
+                    }
+                    if(current == NULL){
+                        wl *newnode = (wl*)malloc(sizeof(wl));
+                        if(newnode != NULL){ 
+                            newnode->word = strdup(cleaned); 
+                            newnode->next = NULL;
+                            newnode->prev = NULL;
+                            newnode->insertionMode = insertionMode;
+                            newnode->wordLength = wlenght(newnode->word); //savearete swsta
+                            newnode->uniqLetters = uniql(newnode->word); 
+                            newnode->numOfTimes = 1;
+                    printf("\nword lenght:%d\n", newnode->wordLength);
+                    printf("%d unique letters\n", newnode->uniqLetters);
+                    printf("insertion mode %d\n", newnode->insertionMode);
+                            current = *wlPtr;
+                            wl *prev = NULL;
+
+                            while(current != NULL && strcmp(cleaned, current->word) > 0){ //compare 'input' me current 'word' na exei thetiko arithmo 
+                                prev = current;                                         //dld na nai megalytero to value tou input se ascii plhktrologio 
+                                current = current->next;                                //alfabhtikh taksinomish
+                            }
+
+                            newnode->next = current;
+                            newnode->prev = prev;
+
+                            if(prev == NULL) 
+                                *wlPtr = newnode;
+                            else prev->next = newnode;
+        
+                            if(current != NULL) 
+                                current->prev = newnode;
+
+                        } else printf( "word not inserted. No memory available.\n");
+                    }
+                }
+            }
+            start = end + 1;
+        }
+        end++;
+    }    
+}
+
+int wlenght(char *word){
+    int i, temp = 0;
+    for(i = 0; word[i] != '\0'; i++){
+        if (isalpha(word[i]))
+            temp++;
+    }
+    return temp;
+}
+
+int uniql(char* word){
+    int i, j, count;
+    char temp[172] = {"\0"};   //h megalyterh leksh sthn ellhnikh
+    for(i = 0; word[i] != '\0'; i++){
+        char c = word[i];
+        if (c >= 'A' && c <= 'Z'){
+            j = c - 'A';
+            if (temp[j] == 0){
+                temp[j] = 1;
+                count++;
+            } 
+        }
+    }
+    return count/2; //trap teleiws kanei th doyleia toy 
+}                   //baraei tis diplasies fores diwrthwse!!!! 
 
 void printList(wl *head)
 {
@@ -103,6 +176,7 @@ void printList(wl *head)
         head = head->next;
     }
 }
+
 
 
 /*
